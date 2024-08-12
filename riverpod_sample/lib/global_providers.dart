ed8926b23b94f2535702521b0add08196d156444
Marinay Wilfredo Genabe Jr <114600451+derf567@users.noreply.github.com>
2024-08-12 15:36:16 +0800

    Add files via upload

---
+++ riverpod_sample/lib/global_providers.dart
@@ -0,0 +1,8 @@
+import 'package:riverpod/riverpod.dart';
+import 'package:riverpod_sample/services/customer_services.dart';
+import 'package:riverpod_sample/services/product_services.dart';
+import 'package:riverpod_sample/services/purchase_service.dart';
+
+final productServiceProvider = Provider((ref) => ProductService(ref));
+final customerServiceProvider = Provider((ref) => CustomerService(ref));
+final purchaseServiceProvider = Provider((ref) => PurchaseService(ref));
